fac37a8fe94df5a386a656160798a06243ce3a37
hamza <hamzacarew@gmail.com>
2019-10-19 22:07:12 +0100

    Added forms for peer reviews

--- peer_review.php
+++ peer_review.php
@@ -7,5 +7,20 @@
     <head>
         <title>Student Peer Reviews</title>
     </head>
-    <body></body>
+    <body>
+        <div class='user_peer'>
+        <center>
+            <form action='' method='POST'>
+            <p> Enter your review:
+            <textarea rows="4" cols="50" name='review_text' placeholder='Enter your review here'>
+            </textarea>
+            </p> <br>
+            <p> Upload an image <br>
+            <input type='file' name='imageUpload'> <br>
+            </p> <br>
+            <input type="submit" value="Upload Image" name="submit">
+        </center>   
+            </form>
+        </div>
+    </body>
 </html>
