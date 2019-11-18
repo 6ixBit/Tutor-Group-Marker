d966f67225019b63e13c9ce05c994e70b7cad281
hamza <hamzacarew@gmail.com>
2019-11-18 11:06:39 +0000

    mod

--- models.php
+++ models.php
@@ -1,24 +1,18 @@
 <?php
-$host = "mysql.cms.gre.ac.uk";
-$user = "mn7754c";
-$password = "root123";
-$db_name = "mdb_mn7754c";
-
-$conn = mysqli_connect($host, $user, $passwd) or die('Failed: '.mysqli_connect_error);
-
-$query = 'SELECT * FROM Groups';
-$result = mysqli_query($conn, $query)
+include 'config.php';
+$conn = mysqli_connect($host, $user, $password, $db_name);
+$query = 'SELECT * FROM Tutor';
+$result = mysqli_query($conn, $query);
+echo mysqli_num_rows($result);
 ?>
 
 <?php
     function calc_average_grade(){
         return NULL;
     }
-
     function get_user(){
         return NULL;
     }
-
     function get_all_groups(){
         return NULL;
     }
