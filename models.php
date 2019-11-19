c2948f35083666f902edb5cad756d7cbafdea3a9
Hamza Nasiru-Carew <mn7754c@gre.ac.uk>
2019-11-19 16:00:01 +0000

    count of group memebers can now be queried

--- models.php
+++ models.php
@@ -10,6 +10,28 @@ include 'config.php';
         return NULL;
     }
 
+	function get_group_count($group_name, $host, $user, $password, $db_name){                              /// Return number of members within a group
+		$conn = mysqli_connect($host, $user, $password, $db_name);
+		$query = "SELECT no_of_users FROM Groups WHERE group_name='$group_name'"; //group_name must be in single quotes for query to work
+		$result = mysqli_query($conn, $query);
+
+		$row = mysqli_fetch_assoc($result);
+		echo $row['no_of_users'];
+	}
+
+	function get_evalutions_count($group_name){    /// Returns number of evaluations in a group
+		
+	}
+
+	function update_group_count($group_name, $host, $user, $password, $db_name){
+	
+	}
+
+	function update_group_evaluations($group_name, $host, $user, $password, $db_name){
+		
+	}
+
+
     function get_all_groups($host, $user, $password, $db_name){
 
 	$conn = mysqli_connect($host, $user, $password, $db_name);
@@ -30,4 +52,5 @@ include 'config.php';
 	return $value;
     }
 
+	get_group_count('Group 3', $host, $user, $password, $db_name);
 ?>
